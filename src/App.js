ddc5fe4f5fadbac07fc61885d24b06912366825a
Virginia <vbernasconi@altimetrik.com>
2022-12-11 19:36:06 -0300

    Add state management to the input

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import './App.css';
-import useState from 'react';
+import { useState } from 'react';
 
 const initialList = [
   {
     id: 'a',
     name: 'Medias',
   },
+
   { id: 'b', name: 'Gorras' },
   { id: 'c', name: 'Remera' },
 ];
 
 function App() {
   const [list, setList] = useState(initialList);
-  const handleChange = () => {};
+  const [name, setName] = useState('');
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
   const handleAdd = () => {};
+
   return (
     <div className='App'>
+      <h1>Regalos:</h1>
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+
       <div>
         <input type='text' onChange={handleChange} />
         <button type='button' onClick={handleAdd}>
           Add
         </button>
       </div>
-      <h1>Regalos:</h1>
-      <ul>
-        {list.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
     </div>
   );
 }
